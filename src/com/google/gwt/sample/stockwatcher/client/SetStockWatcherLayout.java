2e6bc9da0856eb28aa8735a11fea92da9051dbc3
Dennis Lehle <dl043@hdm-stuttgart.de>
2018-02-28 23:40:11 +0100

    Klasse zum setzen des Google Images. (Layout Settings)

---
+++ src/com/google/gwt/sample/stockwatcher/client/SetStockWatcherLayout.java
@@ -0,0 +1,28 @@
+package com.google.gwt.sample.stockwatcher.client;
+
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Mit dieser Klasse wird ein Layout angepasst (Google-Img).
+ * Man könnte dieses Image auch einzeln in die Methode onLoad() hinein schreiben, zu Test zwecken 
+ * wurde dies aber hier realisiert.
+ * @author Dennis Lehle
+ *
+ */
+public class SetStockWatcherLayout {
+	
+	public HorizontalPanel swLayout = new HorizontalPanel();
+	
+	
+	public void setLayout(){
+
+		swLayout.add(new HTML("<img width=\"50%\" src=\"images/Google-Code.jpg\" alt=\"Fehler\">"));
+		swLayout.addStyleName("centerImg");
+
+	}
+
+}
